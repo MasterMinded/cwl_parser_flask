62a1f464143f49288333266a5e2575eb71785b62
Robin <r.m.p.linde@students.uu.nl>
2020-07-14 15:09:37 +0200

    Add boilerplate code for flask service

---
+++ routes/request_api.py
@@ -0,0 +1,26 @@
+import uuid
+from datetime import datetime, timedelta
+from flask import jsonify, abort, request, Blueprint
+
+from validate_email import validate_email
+REQUEST_API = Blueprint('request_api', __name__)
+
+
+def get_blueprint():
+    """Return the blueprint for the main app module"""
+    return REQUEST_API
+
+
+
+
+
+@REQUEST_API.route('/send_file', methods=['POST'])
+def get_records():
+    """Return file metadata
+    """
+
+    if not request.get_json():
+        abort(400)
+    data = request.get_json(force=True)
+    return jsonify(data), 200
+
